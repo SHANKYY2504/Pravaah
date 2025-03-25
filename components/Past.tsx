1699646a4b104b37b4a62224ca6bd64b1dd2d82e
SHANKYY2504 <23ce02033@iitbbs.ac.in>
2025-03-26 04:37:43 +0530

    added events

--- components/Past.tsx
+++ components/Past.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
 const speakers = [
-  "/Nayak.png",
-  "/Omkar.png",
-  "/Brahma.png",
-  "/Tapan.png",
-  "/Manas.png",
-  "/Amar.png",
+  { src: "/jag.png", name: "DR. JAGANNATH NAYAK (DRDO)" },
+  { src: "/omr.png", name: "DR. OMKAR RAI (STARTUP ODISHA)" },
+  { src: "/mishra.png", name: "BRAHMA MISHRA (UCCI)" },
+  { src: "/tkc.png", name: "DR. TAPAN KUMAR CHAND (VEDANTA)" },
+  { src: "/panda.png", name: "SHRI MANAS PANDA (OCAC)" },
+  { src: "/patanaik.png", name: "SHRI DR. AMAR PATANAIK (MP, ODISHA)" },
 ];
 
 const PastSpeakers: React.FC = () => {
@@ -19,17 +19,19 @@ const PastSpeakers: React.FC = () => {
         </h2>
         <div className="h-1 w-20 bg-gradient-to-r from-yellow-300 to-orange-500 mx-auto"></div>
 
-        
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-12  px-4 mt-10">
-          {speakers.map((src, index) => (
-            <div key={index} className="flex justify-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-12 px-4 mt-10">
+          {speakers.map((speaker, index) => (
+            <div key={index} className="flex flex-col items-center">
               <Image
-                src={src}
-                alt={`Speaker ${index + 1}`}
-                width={200} // Bigger images for better visibility
+                src={speaker.src}
+                alt={speaker.name}
+                width={200}
                 height={200}
                 className="rounded-full object-cover w-[100px] h-[100px] sm:w-[140px] sm:h-[140px] md:w-[160px] md:h-[160px] lg:w-[180px] lg:h-[180px]"
               />
+              <p className="text-white text-sm sm:text-base mt-2 text-center max-w-[150px]">
+                {speaker.name}
+              </p>
             </div>
           ))}
         </div>
@@ -39,3 +41,4 @@ const PastSpeakers: React.FC = () => {
 };
 
 export default PastSpeakers;
+
